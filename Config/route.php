afc865a1afd699790f081f02b1a6262dda234cdb
Shiroe_sama <pro.casper805@sfr.fr>
2017-12-04 19:24:08 +0100

    ADD : Fichiers Configs

---
+++ Config/route.php
@@ -0,0 +1,54 @@
+<?php
+	
+	/**
+	 * --------------------------------------------------------------------------
+	 *   @Copyright : License MIT 2017
+	 *
+	 *   @Author : Alexandre Caillot
+	 *   @WebSite : https://www.shiros.fr
+	 *
+	 *   @File : route.php
+	 *   @Created_at : 02/12/2017
+	 *   @Update_at : 02/12/2017
+	 * --------------------------------------------------------------------------
+	 */
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Root Folder
+	|--------------------------------------------------------------------------
+	|
+	| Racine du dossier des Controlleurs.
+	|
+	*/
+
+	$Route['ROOT_FOLDER'] = 'App/Controller';
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Routing
+	|--------------------------------------------------------------------------
+	|
+	| Définition des routes pour l'acces au page
+	|
+	| Exemple :
+	|   - 'accueil/' = "HomeController.index" | Dans le cas où tout les controllers sont dans le même dossier (App/Controller/HomeController.php)
+	|   - 'accueil/' = "Home.HomeController.index" | Dans le cas où les controllers ne sont dans le même dossier (App/Controller/Home/HomeController.php)
+	|
+	*/
+
+	$Route = [
+	
+	
+	/*
+	|--------------------------------------------------------------------------
+	| End Config
+	|--------------------------------------------------------------------------
+	|
+	*/
+	
+	];
+
+	return $Route;
+?>
